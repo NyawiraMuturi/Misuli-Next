261339654616b012b0044bf2a5056dd71da4ff72
NyawiraMuturi <albinamuturi@gmail.com>
2024-04-03 22:09:30 +0300

    chore(use exercise api): successfully make a call and display data from rapid API

--- components/custom/cards/recipeCard.tsx
+++ components/custom/cards/recipeCard.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
 
-export default function RecipeCard() {
+
+const apiKey = process.env.API_KEY || '';
+const apiHost = 'exercisedb.p.rapidapi.com';
+
+
+ async function getExercises (){
+    let response = await fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', {
+        method: 'GET', 
+        headers: {
+            'X-RapidAPI-Key': apiKey,
+            'X-RapidAPI-Host': apiHost
+        }
+    })
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
+      }
+
+      return response.json()
+}
+
+export default async function RecipeCard() {
+    const data = await getExercises()
+    console.log(data)
+
   return (
-    <div>Recipe card</div>
+    <div>
+        {
+            data?.map((bodyPart: any)=>(
+                <div key={bodyPart.id}>
+                    {bodyPart}
+                </div>
+            ))
+        }
+    </div>
   )
 }
+
+
+
+  
 
   
 
